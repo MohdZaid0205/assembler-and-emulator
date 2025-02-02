6960f2ce17e992a026a4e75bbf4eef457398bd28
MohdZaid0205 <mdzaid0205@gmail.com>
2025-02-02 03:30:06 +0000

    Enhance encodings.h with Instruction struct and update Register definition

--- SimpleAssembler/include/encodings.h
+++ SimpleAssembler/include/encodings.h
@@ -1,23 +1,50 @@
 #ifndef REG_ENCODINGS
 #define REG_ENCODINGS
 
+#define byte char
+
 /**
  * @struct Register
  * @brief Defines a structure to represent a processor register.
  *
- * This structure holds:
- * - A string `name`: The register's symbolic name (e.g., "x0", "sp").
- * - An integer `address`: The binary encoding of the register address.
+ * This structure represents a processor register, which typically consists of:
+ * - `name`: A constant character pointer holding the symbolic name of the register (e.g., "x0", "sp").
+ * - `address`: A 5-bit binary integer that encodes the unique address of the register.
  */
 typedef struct {
     const char* name;   // Symbolic name of the register (e.g., "x0")
-    const int address;  // 5-bit binary address of the register
+    const byte address; // 5-bit binary address of the register
 } Register;
+
+/**
+ * @struct Instruction
+ * @brief Represents an instruction in a processor's instruction set architecture.
+ *
+ * This structure holds information necessary to define a complete instruction:
+ * - `name`: The symbolic name of the instruction (e.g., "ADD", "SUB").
+ * - `type`: The symbolic name of instruction type (e.g., "R", "I").
+ * - `funct7`: A 7-bit function code used to specify additional operation details.
+ * - `funct3`: A 3-bit function code used for operation differentiation.
+ * - `opcode`: A unique operation code representing the instruction class.
+ * - `immediate`: A constant integer that may be used as an immediate operand for certain instructions.
+ */
+typedef struct {
+    const char* name;    // Symbolic name of the instruction (e.g., "ADD")
+    const char* type;    // Symbolic name of instruction type (e.g., "R", "I")
+    const byte funct7;   // 7-bit function code for additional operation details
+    const byte funct3;   // 3-bit function code for operation differentiation
+    const byte opcode;   // Operation code representing the instruction class
+    const int immidiate; // Immediate operand used by certain instructions
+} Instruction;
+
 
 // An array of Register structs representing the 32 general-purpose registers.
 // Defined in encodings.c
 extern Register Encodings[];
 extern Register ABI_names[];
+
+// An array containing all possible instructions and their corresponding feilds.
+extern Instruction Instructions[];
 
 /**
  * @brief Retrieves the binary address of a given register name.

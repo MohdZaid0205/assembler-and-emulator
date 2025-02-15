639c86fc53a83e061f856b7170eedba042da00ab
Mohd Zaid <mdzaid0205@gmail.com>
2025-02-15 14:41:56 +0530

    Fix typo in variable name for assembly source
    Corrected the variable name from `__assembly_ommit` to `__assembly_omit` to address a typographical error in the code.

--- SimpleAssembler/assembler.c
+++ SimpleAssembler/assembler.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv){
     // [<simple_assembler> source_file.asm -o ommition_file.*] * ->  format not yet decided
     const char* __assembler_name  = argv[0];
     const char* __assembly_source = argv[1];
-    const char* __assembly_ommit  = argv[3];
+    const char* __assembly_omit  = argv[3];
 
     return 0;
 }

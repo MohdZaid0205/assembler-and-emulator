5503911d482c61d0281572a419708cd640b7f1ca
Mohd Zaid <mdzaid0205@gmail.com>
2025-02-18 19:26:37 +0530

    Add implementation notes for analysis functions
    Added comments in the `syntacticalAnalysis` and `lexicalAnalysis` functions to indicate that these functions are to be implemented by Shlok and Rishabh, respectively. This clarifies the intended future work on these functions.
    
    Co-Authored by: Th3D3stroy3r <shlok24535@iiitd.ac.in>

--- SimpleAssembler/source/analysis.c
+++ SimpleAssembler/source/analysis.c
@@ -1,9 +1,11 @@
 #include "analysis.h"
 
 bool syntacticalAnalysis(FILE* file) {
+	// To be implimented by shlok.
 	return true;
 }
 
 bool lexicalAnalysis(FILE* file) {
+	// To be implimented by rishabh.
 	return false;
 }

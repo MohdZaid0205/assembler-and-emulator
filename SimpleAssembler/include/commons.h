614ce890f5c8f823c43319823eb0e16a2270c40a
RishabhGuptaGH <Rishabh24461@iiiitd.ac.in>
2025-02-19 14:07:13 +0530

    Add string_splitter function for word splitting
    Updated `commons.h` to declare the `string_splitter` function, which splits a string into words based on specified delimiters. Implemented the function in `commons.c`, handling word detection and storage in an output array, with proper management of word boundaries and termination.

--- SimpleAssembler/include/commons.h
+++ SimpleAssembler/include/commons.h
@@ -3,4 +3,14 @@
 
 #include "pch.h"
 
+/**
+ * @breif Splits a string into words.
+ *
+ * This function splits a string into words based on the delimiters provided.
+ * @param input The string to be split.
+ * @param words The array of words into which the string will be split.
+ * @param delimiters The delimiters based on which the string will be split.
+ */
+void string_splitter(char input[], char words[10][10], char delimiters[]);
+
 #endif
--- SimpleAssembler/source/commons.c
+++ SimpleAssembler/source/commons.c
@@ -1,1 +1,31 @@
 #include "commons.h"
+
+void string_splitter(char input[], char words[10][10], char delimiters[]) {
+
+    int wordsCount = 0;
+    int wordIndex = 0;
+    char word[10];
+    int len = strlen(input);
+
+    for (int i = 0; i < len; i++) {
+        if (strchr(delimiters, input[i]) != NULL) {
+            if (wordIndex > 0) {
+                word[wordIndex] = '\0';
+                strcpy(words[wordsCount], word);
+                wordsCount++;
+                wordIndex = 0;
+            }
+        }
+        else {
+            word[wordIndex++] = input[i];
+        }
+    }
+
+    if (wordIndex > 0) {
+        word[wordIndex] = '\0';
+        strcpy(words[wordsCount], word);
+        wordsCount++;
+    }
+
+    words[wordsCount][0] = '\0';
+}

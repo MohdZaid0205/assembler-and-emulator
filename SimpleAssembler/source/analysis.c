c5b12599fc0ff7c494f9cf39f1153c564f8f4355
RishabhGuptaGH <Rishabh24461@iiitd.ac.in>
2025-02-19 17:23:42 +0530

    Refactor isLineLexicallyCorrect for clarity and efficiency
    Improved the structure and readability of the `isLineLexicallyCorrect` function in `analysis.c`.
    Key updates include:
    - Moved initial validity checks to the start and simplified logic.
    - Streamlined handling of instruction types (R, I, S, B, J) to return results directly.
    - Established a consistent approach for validating registers and immediate values.
    - Removed redundant code for clearer and more maintainable logic.

--- SimpleAssembler/source/analysis.c
+++ SimpleAssembler/source/analysis.c
@@ -61,69 +61,56 @@ bool isLineLexicallyCorrect(Line line) {
 		return true;
 	}
 
+	const char* lineContent = line.content;
+	char words[10][10];
+	char delimiters[] = {' ',','};
+	string_splitter(lineContent, words, delimiters);
 
-	else if (lineType == TEXTS) {
-		const char* lineContent = line.content;
-		char words[10][10];
-		char delimiters[] = {' ',','};
-		string_splitter(lineContent, words, delimiters);
-
-		if (!is_valid_instruction(words[0]))
-			return false;
+	if (!is_valid_instruction(words[0]))
+		return false;
 		
-		Instruction instruction1 = instruction_encoding(words[0]);
-		const char* instructionType = instruction1.type;
-
-		if (strcmp(instructionType, "R") == 0) {
-			bool s1 = is_valid_register(words[1]);
-			bool s2 = is_valid_register(words[2]);
-			bool s3 = is_valid_register(words[3]);
-			if (s1 && s2 && s3 == false) {// To impliment which type of error is raised.
-			    return false;
-			}
-		}
-		
-		if (strcmp(instructionType, "I") == 0) {
-			bool s1 = is_valid_register(words[1]);
-			bool s2 = is_valid_register(words[2]);
-			bool s3 = is_valid_immediate(words[3], I);
-
-			if (s1 && s2 && s3 == false) {// To impliment which type of error is raised.
-				return false;
-			}
-		}
-
-		if (strcmp(instructionType, "S") == 0) {
-			char delimiters2[] = { '(', ')'};
-			char words2[10][10];
-			string_splitter(words[2], words2, delimiters2);
-			bool s1 = is_valid_register(words[1]);
-			bool s2 = is_valid_immediate(words2[0], S);
-			bool s3 = is_valid_register(words2[1]);
-			if (s1 && s2 && s3 == false) {// To impliment which type of error is raised.
-				return false;
-			}
-		}
-
-		if (strcmp(instructionType, "B") == 0) {
-			bool s1 = is_valid_register(words[1]);
-			bool s2 = is_valid_register(words[2]);
-			bool s3 = is_valid_immediate(words[3], B) || is_valid_label(words[3]);
-			if (s1 && s2 && s3 == false) {// To impliment which type of error is raised.
-				return false;
-			}
-		}
-
-		if (strcmp(instructionType, "J") == 0) {
-			bool s1 = is_valid_register(words[1]);
-			bool s2 = is_valid_immediate(words[2], J) || is_valid_label(words[2]);
-			if (s1 && s2 == false) {// To impliment which type of error is raised.
-				return false;
-			}
-		}
-		
-		return true;
+	Instruction instruction1 = instruction_encoding(words[0]);
+	const char* instructionType = instruction1.type;
+	
+	if (strcmp(instructionType, "R") == 0) {
+		bool s1 = is_valid_register(words[1]);
+		bool s2 = is_valid_register(words[2]);
+		bool s3 = is_valid_register(words[3]);
+		return s1 && s2 && s3;
 	}
+		
+	if (strcmp(instructionType, "I") == 0) {
+		bool s1 = is_valid_register(words[1]);
+		bool s2 = is_valid_register(words[2]);
+		bool s3 = is_valid_immediate(words[3], I);
+		return s1 && s2 && s3;
+	}
+
+	if (strcmp(instructionType, "S") == 0) {
+		char delimiters2[] = { '(', ')'};
+		char words2[10][10];
+		string_splitter(words[2], words2, delimiters2);
+		bool s1 = is_valid_register(words[1]);
+		bool s2 = is_valid_immediate(words2[0], S);
+		bool s3 = is_valid_register(words2[1]);
+		return s1 && s2 && s3;
+
+	}
+
+	if (strcmp(instructionType, "B") == 0) {
+		bool s1 = is_valid_register(words[1]);
+		bool s2 = is_valid_register(words[2]);
+		bool s3 = is_valid_immediate(words[3], B) || is_valid_label(words[3]);
+		return s1 && s2 && s3;
+	}
+
+	if (strcmp(instructionType, "J") == 0) {
+		bool s1 = is_valid_register(words[1]);
+		bool s2 = is_valid_immediate(words[2], J) || is_valid_label(words[2]);
+		return s1 && s2;
+	}
+		
+	return true;
 
 
 }
